f25b82418bea9333537b19bf8cbab0169fe09538
Lorezi <onaulogholawrence@gmail.com>
2021-08-23 02:09:06 +0100

    adds comments

--- src/shared/utils/transferSchema.ts
+++ src/shared/utils/transferSchema.ts
@@ -1,6 +1,12 @@
 import Joi from "joi";
 import { validateRequest } from "./Validation";
 
+/**
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
 const transferSchema = (req: any, res: any, next: any) => {
   const schema = Joi.object({
     account_no: Joi.string().required(),
